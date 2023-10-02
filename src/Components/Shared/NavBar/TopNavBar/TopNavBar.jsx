5f208e8f78ba8790f542c8aa50d317e05a1147a0
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-02 17:51:43 +0600

    top navbar options were added

--- src/Components/Shared/NavBar/TopNavBar/TopNavBar.jsx
+++ src/Components/Shared/NavBar/TopNavBar/TopNavBar.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import Logo from './logo';
+import TopNavOptions from './TopNavOptions';
 
 const TopNavBar = () => {
-
-    const navOptions = <>
-        <li><a>About Us</a></li>
-        <li><a>Blog</a></li>
-        <li><a>Contact Us</a></li>
-        <li><a>Help & Support</a></li>
-    </>
 
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost md:hidden lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        {navOptions}
-                    </ul>
-                </div>
                 <Logo />
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {navOptions}
-                </ul>
+                {/* menu options were here */}
+                <TopNavOptions />
             </div>
             <div className="navbar-end">
                 <a className="btn">Button</a>
