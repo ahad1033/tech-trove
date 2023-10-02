d5ee12b221410301fee187fe9774844f268e9a53
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-02 17:40:21 +0600

    top navbar created and logo added to the nav bar

---
+++ src/Components/Shared/NavBar/TopNavBar/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+           <h1 className='text-3xl'>Tech<span className='font-bold'>Trove</span></h1> 
+        </div>
+    );
+};
+
+export default Logo;
