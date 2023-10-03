fcbaa0ef3ac309c594d696a3a67f80c830c0fc1e
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-03 13:33:36 +0600

    code modified to beautify UI

--- src/Components/Shared/NavBar/TopNavBar/SearchOption.jsx
+++ src/Components/Shared/NavBar/TopNavBar/SearchOption.jsx
@@ -4,18 +4,18 @@ import { AiOutlineDown } from 'react-icons/ai';
 
 const SearchOption = () => {
     return (
-        <div className='lg:flex md:flex sm:hidden items-center justify-center border border-1 rounded-xl px-1 gap-2'>
-            <input type="text" placeholder="Search Products" className="input" />
+        <div className='lg:flex md:flex sm:hidden items-center justify-center border border-1 rounded-xl gap-2'>
+            <input type="text" placeholder="Search Products" className="input-xs input-ghost pl-2 border-none" />
             <details className="dropdown">
-                <summary className="mx-1 text-xs btn normal-case whitespace-nowrap flex items-center">
-                    All Categories <AiOutlineDown className="ml-1" />
+                <summary className="mx-1 text-xs btn normal-case whitespace-nowrap">
+                    <h4 className='flex flex-row items-center gap-2'>All Categories<span className="ml-1"><AiOutlineDown /></span></h4>
                 </summary>
                 <ul className="mt-2 p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
                     <li><a>Item 1</a></li>
                     <li><a>Item 2</a></li>
                 </ul>
             </details>
-            <FaSearch className='text-3xl pl-2 mx-2 border-s-1' />
+            <FaSearch className='text-3xl px-2 mx-2 border-s-2' />
         </div>
     );
 };
