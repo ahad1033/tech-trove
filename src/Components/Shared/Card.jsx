96a6bb721139b11e52117042b61911188f265f39
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-03 11:38:41 +0600

    cart component added

---
+++ src/Components/Shared/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HiOutlineShoppingBag } from 'react-icons/hi';
+
+const Card = () => {
+    return (
+        <div>
+            <p className='flex flex-row gap-2'><span><HiOutlineShoppingBag /></span>Card</p>
+        </div>
+    );
+};
+
+export default Card;
