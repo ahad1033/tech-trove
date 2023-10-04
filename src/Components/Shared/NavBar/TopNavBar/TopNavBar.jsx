48f383611f2d3cbc7c00db10df6ef7e3449a3f91
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-05 03:15:00 +0600

    UI modified

--- src/Components/Shared/NavBar/TopNavBar/TopNavBar.jsx
+++ src/Components/Shared/NavBar/TopNavBar/TopNavBar.jsx
@@ -7,14 +7,14 @@ import SocialIcons from './SocialIcons';
 const TopNavBar = () => {
 
     return (
-        <div className="navbar lg:flex-row md:flex-col sm:flex-row-reverse lg:justify-between md:justify-between sm:justify-between bg-base-100 section-container lg:py-7 md:py-3 sm:py-8">
+        <div className="navbar lg:flex-row md:flex-col sm:flex-row-reverse lg:justify-between md:justify-between sm:justify-between bg-white text-black section-container lg:py-7 md:py-3 sm:py-8 ">
             <div className="w-full">
                 <div className="flex lg:flex-row md:flex-row gap-3 items-center justify-between">
                     <Logo />
                     <SearchOption />
                 </div>
             </div>
-            <div className="flex justify-between w-full">
+            <div className="flex md:mt-4 justify-between w-full ">
                 <TopNavOptions />
                 <SocialIcons />
             </div>
--- src/Components/Shared/NavBar/TopNavBar/TopNavOptions.jsx
+++ src/Components/Shared/NavBar/TopNavBar/TopNavOptions.jsx
@@ -15,12 +15,12 @@ const TopNavOptions = () => {
                 <label tabIndex={0} className="btn btn-ghost md:hidden lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-semibold text-sm">
                     {navOptions}
                 </ul>
             </div>
             <div className="lg:flex md:flex sm:hidden flex-row">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 font-bold text-sm">
                     {navOptions}
                 </ul>
             </div>
