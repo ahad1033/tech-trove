c3b748d8ff9992d09be95d5298f8cdd268b9ef4a
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-05 03:04:58 +0600

    category nav bar added

---
+++ src/Components/Shared/NavBar/CategoryNav/CategoryNav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CategoryNav = () => {
+    return (
+        <div className='section-container bg-white mt-7 py-2'>
+             <ul className='lg:flex md:flex lg:flex-row md:flex-row sm:hidden justify-between  text-black font-semi-bold '>
+                <a href=""><li>Woman</li></a>
+                <a href=""><li>Male</li></a>
+                <a href=""><li>Mother-Child</li></a>
+                <a href=""><li>Home & Furniture</li></a>
+                <a href=""><li>Super Market</li></a>
+                <a href=""><li>Cosmetics</li></a>
+                <a href=""><li>Shoes & Bags</li></a>
+                <a href=""><li>Electronic</li></a>
+                <a href=""><li>Sport & Outdoor</li></a>
+                <a href=""><li>Best Seller</li></a>
+             </ul>
+        </div>
+    );
+};
+
+export default CategoryNav;
--- src/Components/Shared/NavBar/NavBar.jsx
+++ src/Components/Shared/NavBar/NavBar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TopNavBar from './TopNavBar/TopNavBar';
 import BottomNavBar from './BottomNavBar/BottomNavBar';
+import CategoryNav from './CategoryNav/CategoryNav';
 
 const NavBar = () => {
     return (
         <div>
             <TopNavBar />
             <BottomNavBar />
+            <CategoryNav />
         </div>
     );
 };
