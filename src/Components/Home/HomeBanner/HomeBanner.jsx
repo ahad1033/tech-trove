31e0861de59de56d0e635eff5887cacd9febc956
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-05 11:02:26 +0600

    banner images added

--- src/Components/Home/HomeBanner/HomeBanner.jsx
+++ src/Components/Home/HomeBanner/HomeBanner.jsx
@@ -3,8 +3,26 @@ import React from 'react';
 const HomeBanner = () => {
     return (
         <div className='flex'>
-            <div className='w-1/3'><img src='https://i.ibb.co/hXjcCxW/banner-Left.jpg' alt="" /></div>
-            <div className='w-2/3'><img className='max-h-fit' src='https://i.ibb.co/kyFFSzs/banner-Right.png' alt="" /></div>
+            <div className='w-1/3'>
+                <img
+                    src='https://i.ibb.co/hXjcCxW/banner-Left.jpg'
+                    alt=""
+                    className='h-full object-cover'
+                />
+            </div>
+            <div className='w-2/3'>
+                <div
+                    className='h-full object-cover'
+                    style={{
+                        background: 'url(<path-to-image>)',
+                        borderRadius: 'var(--spacing-0, 0px)',
+                        backgroundPosition: '50%',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: 'var(--button-default, #4172DC)',
+                    }}
+                ></div>
+            </div>
         </div>
     );
 };
