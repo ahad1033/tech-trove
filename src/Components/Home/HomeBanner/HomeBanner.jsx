99d60e45bafa71e9f355a4ffab5d077e8f597fd5
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-05 17:25:34 +0600

    summer essential component added for the home banner

--- src/Components/Home/HomeBanner/HomeBanner.jsx
+++ src/Components/Home/HomeBanner/HomeBanner.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { TbShoppingBagPlus } from 'react-icons/tb';
 
 const HomeBanner = () => {
     return (
         <div className='flex'>
-            <div className='w-1/3'>
+            <div className='w-1/3 relative'>
                 <img
                     src='https://i.ibb.co/hXjcCxW/banner-Left.jpg'
                     alt=""
                     className='h-full object-cover'
                 />
+                <div className='hidden lg:block absolute left-[433px] bottom-[148px]'>
+                    <div className='flex flex-col items-center justify-center bg-white gap-2 py-8 px-8'>
+                        <h1 className='text-xl font-bold text-black'>Summer Essentials</h1>
+                        <h3 className='text-xl' style={{ color: '#FF2E00' }}>20% off</h3>
+                    </div>
+                    <div className='bottom-0 bg-black py-1'>
+                        <p className='font-white w-full text-center'>19 Jul-30 Jul</p>
+                    </div>
+                </div>
             </div>
             <div className='w-2/3'>
                 <div
@@ -24,7 +34,7 @@ const HomeBanner = () => {
                 >
                     <h1 className='text-4xl uppercase '>Kimonos, Caftans & Pareos</h1>
                     <h2 className=''>Poolside glam included From $4.99</h2>
-                    <button className="button-primary">Shop Now</button>
+                    <button className="button-primary"><TbShoppingBagPlus />Shop Now</button>
                 </div>
             </div>
         </div>
