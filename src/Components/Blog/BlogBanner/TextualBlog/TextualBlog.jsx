3086340ef8b2b2fea23ec85f82043126eb00b66d
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-10 03:13:00 +0600

    textual blog section added to the blog page

---
+++ src/Components/Blog/BlogBanner/TextualBlog/TextualBlog.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const TextualBlog = () => {
+    return (
+        <div className='section-container my-14 text-justify'>
+            <h1 className='text-2xl font-bold border-l-4 border-purple-600  px-5'>
+                "Unveiling Timeless Elegance: Exploring the Allure of Vintage Fashion"
+            </h1>
+            <p className='text-base mt-7  px-5 leading-7'>
+                The Essence of Time Travel
+                Vintage fashion allows us to transcend eras, stepping into the shoes of those who came before us. Each piece tells a story, offering a glimpse into the cultural and societal norms of its time. From the flapper dresses of the 1920s that embodied the spirit of rebellion to the tailored suits of the 1960s that epitomized sophistication, every era has its signature style waiting to be revived.
+                Quality Beyond Compare
+                One of the hallmarks of vintage fashion is the unparalleled craftsmanship that went into creating garments that stood the test of time. In a world where fast fashion often prioritizes quantity over quality, vintage pieces remind us of an era when attention to detail and durability were paramount. The feel of a vintage silk dress or the weight of a hand-sewn coat is a testament to the dedication of artisans of yesteryears.
+                Eclectic Personal Style
+                Embracing vintage fashion allows us to curate a wardrobe that's uniquely ours. Mixing and matching vintage pieces with contemporary items creates a personal style that defies categorization. From pairing a 1970s bohemian blouse with modern jeans to accessorizing a little black dress with vintage brooches, the possibilities are endless and the results are often strikingly original.
+                Sustainable Chic
+                As the world embraces sustainability, vintage fashion takes center stage as a prime example of eco-conscious style. Choosing pre-loved clothing not only reduces the demand for new production but also breathes new life into garments that might otherwise be forgotten. It's a nod to slow fashion, a movement that encourages thoughtful consumption and reduces the impact on the environment.
+            </p>
+        </div>
+    );
+};
+
+export default TextualBlog;
--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BlogBanner from '../../Components/Blog/BlogBanner/BlogBanner';
+import TextualBlog from '../../Components/Blog/BlogBanner/TextualBlog/TextualBlog';
 
 const Blog = () => {
     return (
         <div>
             <BlogBanner />
+            <TextualBlog />
         </div>
     );
 };
