73d37eac980c9ad4bc84bbf80e35906aedbb4a71
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-12 03:14:18 +0600

    galary section added to the blog page

---
+++ src/Components/Blog/BlogGalaray/BlogGalaray.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const BlogGalaray = () => {
+    return (
+        <div className='section-container w-full flex justify-center gap-5 lg:my-16 mg:my-10 sm:m-8'>
+            <div className='w-2/3'>
+                <img className='w-full h-full' src="https://i.ibb.co/VgjVgY0/blog-Galary1.png" alt="" />
+            </div>
+            <div className='w-1/3 flex flex-col gap-5'>
+                <img className='w-full' src="https://i.ibb.co/6nrV9Xj/blog-Galary2.png" alt="" />
+                <img className='w-full' src="https://i.ibb.co/y007txw/blog-Galary4.png" alt="" />
+                <img className='w-full' src="https://i.ibb.co/gvTKcP7/blog-Galary3.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default BlogGalaray;
--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -3,12 +3,14 @@ import BlogBanner from '../../Components/Blog/BlogBanner/BlogBanner';
 import TextualBlog from '../../Components/Blog/TextualBlog/TextualBlog';
 import BlogBottom from '../../Components/Blog/BlogBottom/BlogBottom';
 import BlogFooter from '../../Components/Blog/BlogFooter/BlogFooter';
+import BlogGalaray from '../../Components/Blog/BlogGalaray/BlogGalaray';
 
 const Blog = () => {
     return (
         <div>
             <BlogBanner />
             <TextualBlog />
+            <BlogGalaray />
             <BlogBottom />
             <BlogFooter />
         </div>
