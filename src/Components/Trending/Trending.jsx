cc3fdfd044f8e129eae50eab734b2cb34c8206be
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-16 23:27:05 +0600

    trending are added to the trending section

---
+++ src/Components/Trending/Trending.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Trending = () => {
+    return (
+        <div>
+            <img src="https://i.ibb.co/f4pC4jQ/4-org-zoom-1.png" alt="" />
+            <div className='flex bottom-0 border-t-black text-white'>
+                <div>
+                    <h1>Cool & Sexy Calvin Klein</h1>
+                    <p>Dotted dress-Casual</p>
+                </div>
+                <div>
+                    <button className='btn btn-outline text-white'>$89 Shop Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Trending;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import HomeBanner from '../../Components/Home/HomeBanner/HomeBanner';
 import Footer from '../../Components/Shared/Footer/Footer';
 import BottomSection from '../../Components/Home/BottomSection/BottomSection';
 import FlashSale from '../../Components/Home/FlashSale/FlashSale';
+import Trending from '../../Components/Trending/Trending';
 
 const Home = () => {
     return (
         <div>
             <HomeBanner />
             <FlashSale />
+            <Trending />
             <BottomSection />
             <Footer />
         </div>
