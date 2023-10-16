a650a8da240e3d2611c9defeb4baa8cd2cb2e60d
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-16 23:30:26 +0600

    trending cards modified

--- src/Components/Trending/Trending.jsx
+++ src/Components/Trending/Trending.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 const Trending = () => {
     return (
         <div>
-            <img src="https://i.ibb.co/f4pC4jQ/4-org-zoom-1.png" alt="" />
-            <div className='flex bottom-0 border-t-black text-white'>
-                <div>
-                    <h1>Cool & Sexy Calvin Klein</h1>
-                    <p>Dotted dress-Casual</p>
-                </div>
-                <div>
-                    <button className='btn btn-outline text-white'>$89 Shop Now</button>
+            <div className='w-96'>
+                <img src="https://i.ibb.co/f4pC4jQ/4-org-zoom-1.png" alt="" />
+                <div className='flex justify-between bottom-0 text-white p-4' style={{backgroundColor: '#262626'}}>
+                    <div>
+                        <h1>Cool & Sexy Calvin Klein</h1>
+                        <p>Dotted dress-Casual</p>
+                    </div>
+                    <div>
+                        <button className='btn btn-outline text-white'>$89 Shop Now</button>
+                    </div>
                 </div>
             </div>
         </div>
