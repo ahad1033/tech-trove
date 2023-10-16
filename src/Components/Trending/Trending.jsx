a4a4508cd8db53153b7a9a9cc026ec0fad414711
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-17 01:00:18 +0600

    trending section created for the home page

--- src/Components/Trending/Trending.jsx
+++ src/Components/Trending/Trending.jsx
@@ -5,15 +5,41 @@ const Trending = () => {
     return (
         <div className='section-container'>
             <SectionTitle title={`Trending Must Have`} />
-            <div className='w-96'>
-                <img src="https://i.ibb.co/f4pC4jQ/4-org-zoom-1.png" alt="" />
-                <div className='flex justify-between bottom-0 text-white p-4' style={{backgroundColor: '#262626'}}>
-                    <div>
-                        <h1>Cool & Sexy Calvin Klein</h1>
-                        <p>Dotted dress-Casual</p>
+            <div className='flex justify-center gap-20'>
+                <div className='w-96'>
+                    <img src="https://i.ibb.co/f4pC4jQ/4-org-zoom-1.png" alt="" />
+                    <div className='flex justify-between bottom-0 text-white p-4' style={{ backgroundColor: '#262626' }}>
+                        <div>
+                            <h1>Cool & Sexy Calvin Klein</h1>
+                            <p>Dotted dress-Casual</p>
+                        </div>
+                        <div>
+                            <button className='btn btn-outline text-white'>$89 Shop Now</button>
+                        </div>
                     </div>
-                    <div>
-                        <button className='btn btn-outline text-white'>$89 Shop Now</button>
+                </div>
+                <div className='w-96'>
+                    <img src="https://i.ibb.co/6RQhftg/4-org-zoom-1.png" alt="" />
+                    <div className='flex justify-between bottom-0 text-white p-4' style={{ backgroundColor: '#262626' }}>
+                        <div>
+                            <h1>Cool & Sexy Calvin Klein</h1>
+                            <p>Dotted dress-Casual</p>
+                        </div>
+                        <div>
+                            <button className='btn btn-outline text-white'>$105 Shop Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-96'>
+                    <img src="https://i.ibb.co/Pt6Qwx2/4-org-zoom-1.png" alt="" />
+                    <div className='flex justify-between bottom-0 text-white p-4' style={{ backgroundColor: '#262626' }}>
+                        <div>
+                            <h1>beige coat Zara</h1>
+                            <p>Cream-Brown-Formal</p>
+                        </div>
+                        <div>
+                            <button className='btn btn-outline text-white'>$70 Shop Now</button>
+                        </div>
                     </div>
                 </div>
             </div>
