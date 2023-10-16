2d618b458733ceaaada77a0a713a7d83684a6df4
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-17 03:28:51 +0600

    Blog UI modified

--- src/Components/Blog/BlogCard/BlogCard.jsx
+++ src/Components/Blog/BlogCard/BlogCard.jsx
@@ -4,7 +4,7 @@ import BottomCard from './BottomCard';
 
 const BlogCard = () => {
     return (
-        <section className='lg:px-28 md:px-12 sm:p-10 flex flex-col justify-center my-16 w-full mx-auto'>
+        <section className='flex justify-center lg:px-28 md:px-12 sm:p-10 my-16'>
             {/* <TopCard /> */}
             <BottomCard />
         </section>
--- src/Components/Blog/BlogCard/BottomCard.jsx
+++ src/Components/Blog/BlogCard/BottomCard.jsx
@@ -3,7 +3,7 @@ import { BiComment } from 'react-icons/bi'
 
 const BottomCard = () => {
     return (
-        <div className='mt-5 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 justify-center gap-10'>
+        <div className='mt-5 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap-20'>
             <div className="w-96 bg-base-100 shadow-sm">
                 <figure><img className='w-full' src="https://i.ibb.co/QMq09cY/photo.png" alt="card" /></figure>
                 <div className="card-body">
@@ -49,6 +49,30 @@ const BottomCard = () => {
                     <div className="flex text-gray-400 mt-1">
                         <p>3 hours ago</p>
                         <p className='flex items-center gap-2 justify-end'><BiComment />21</p>
+                    </div>
+                </div>
+            </div>
+            <div className="w-96 bg-base-100 shadow-sm">
+                <figure><img className='w-full' src="https://i.ibb.co/0Mx2xXR/photo.png" alt="card" /></figure>
+                <div className="card-body">
+                    <p className='text-orange-500'>tech</p>
+                    <h2 className="card-title">Aptkdoe Smartwatch Women Men </h2>
+                    <p className='text-justify'>It seems like you've mentioned "Aptkdoe Smartwatch" in combination with "Women" and "Men." However, as of my last knowledge update in September 2021, I don't have specific...</p>
+                    <div className="flex text-gray-400 mt-1">
+                        <p>4 hours ago</p>
+                        <p className='flex items-center gap-2 justify-end'><BiComment />56</p>
+                    </div>
+                </div>
+            </div>
+            <div className="w-96 bg-base-100 shadow-sm sm:hide">
+                <figure><img className='w-full' src="https://i.ibb.co/NTkhLKF/photo.png" alt="card" /></figure>
+                <div className="card-body">
+                    <p className='text-orange-500'>tech</p>
+                    <h2 className="card-title">Anker 325 power bank</h2>
+                    <p className='text-justify'>Anker 325 power bank, 20000mAh external battery PowerIQ technology USB-C port, enormous energy density, compatible with iPhone, Samsung Galaxy, iPad and more...</p>
+                    <div className="flex text-gray-400 mt-1">
+                        <p>4 hours ago</p>
+                        <p className='flex items-center gap-2 justify-end'><BiComment />10</p>
                     </div>
                 </div>
             </div>
