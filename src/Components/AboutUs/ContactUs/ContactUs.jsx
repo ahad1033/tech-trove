bd83c37d0d85c69c80ed0b59bbd4c264b4a4facc
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-18 04:24:56 +0600

    concern form modified of the about us page

--- src/Components/AboutUs/ContactUs/ContactUs.jsx
+++ src/Components/AboutUs/ContactUs/ContactUs.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 
 const ContactUs = () => {
     return (
-        <div className='section-container'>
-            <div className='w-1/2 p-14' style={{ backgroundColor: '#262626' }}>
+        <div className='section-container flex flex-row'>
+            <div className='w-1/2 p-14 relative' style={{ backgroundColor: '#262626' }}>
                 <h2 className='text-5xl font-bold text-white'>Tell us about your <p className='mt-3' style={{ color: '#FFC94B' }}>Concerns</p></h2>
-                <div className="form-control w-full max-w-xs mt-8">
+                <div className="form-control w-full mt-8">
                     <label className="label">
-                        <span className="label-text text-white mb-3">Your name</span>
+                        <span className="label-text text-white mb-3">Your Name</span>
                     </label>
                     <div>
                         {/* <AiOutlineUser /> */}
-                        <input type="text" placeholder="Enter Your Name Here" className="input input-bordered border-white max-w-full" />
+                        <input type="email" placeholder="Enter Your Name Here" className="input input-bordered border-white w-full" />
                     </div>
                 </div>
-                <div className="form-control w-full max-w-xs mt-8">
+                <div className="form-control w-full mt-8">
                     <label className="label">
                         <span className="label-text text-white mb-3">Email</span>
                     </label>
@@ -30,10 +30,10 @@ const ContactUs = () => {
                     </label>
                     <textarea className="textarea textarea-bordered h-24 border-white" placeholder="Tell Us About Your Conceerns"></textarea>
                 </div>
-                <button className='btn button-primary-wide mt-10'>Send</button>
+                <button className='btn button-primary-wide mt-10' >Send</button>
             </div>
-            <div className='w-1/2'>
-
+            <div className='w-1/2 relative overflow-hidden'>
+                <div className="h-full" style={{ background: `url('https://i.ibb.co/hXjcCxW/banner-Left.jpg') center/cover no-repeat` }}></div>
             </div>
         </div>
     );
