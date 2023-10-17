bdf189fc7b781dfcf02b016c487a052dff71c3d7
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-18 02:33:49 +0600

    make responsive the top about us page

--- src/Components/AboutUs/TopAbout/TopAbout.jsx
+++ src/Components/AboutUs/TopAbout/TopAbout.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const TopAbout = () => {
     return (
-        <div className='section-container flex items-center my-10 gap-36'>
+        <div className='section-container flex items-center my-10 sm:gap-10 md:gap-20 lg:gap-36'>
             <div className='lg:w-2/3 md:w-2/3 sm:w-1/2'>
-                <p className='text-2xl sm:text-lg mb-4'>Let's get to know TechTrove</p>
-                <h1 className='text-5xl sm:text-3xl leading-normal'>Providing the best experience to make your <span style={{color: '#3E60C9'}}>Online Shopping</span> </h1>
+                <p className='sm:text-lg md:text-2xl lg:text-2xl mb-4'>Let's get to know TechTrove</p>
+                <h1 className='sm:text-3xl md:text-3xl lg:text-6xl leading-normal'>Providing the best experience to make your <span style={{color: '#3E60C9'}}>Online Shopping</span> </h1>
                 <p className='my-6 sm:my:3 text-justify leading-10 sm:leading-7'>At Luminae, we are more than just an online store – we are your ultimate destination for an unparalleled shopping experience. Our journey began with a simple yet powerful idea: to create a platform that not only offers a wide array of products but also fosters a sense of community and connection among our customers.</p>
                 <div className='flex flex-row justify-between items-center gap-10'>
                     <div className='text-left'>
@@ -22,8 +22,8 @@ const TopAbout = () => {
                     </div>
                 </div>
             </div>
-            <div className='w-2/3 sm:w-1/2'>
-                <h3>We have made many people satisfied with our Platform</h3>
+            <div className='flex sm:w-1/2 md:w-1/3 lg:w-1/3 justify-center items-center'>
+                <h3 className='flex items-center w-80 h-44 text-center text-xl font-semibold p-10 leading-9 rounded-2xl' style={{backgroundColor:'#EAF3FB', color:'#555555'}}>We have made many people satisfied with our Platform</h3>
             </div>
         </div>
     ); 
