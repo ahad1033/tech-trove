3781d27a34243c49cf6d1e5d4c833eb3f3a4cca3
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-18 02:20:11 +0600

    about us to section modified

--- src/Components/AboutUs/TopAbout/TopAbout.jsx
+++ src/Components/AboutUs/TopAbout/TopAbout.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 
 const TopAbout = () => {
     return (
-        <div className='section-container flex items-center'>
-            <div className='lg:w-2/3 md:w-2/3 sm:w-1/2 '>
-                <p className='text-2xl mb-4'>Let's get to know TechTrove</p>
-                <h1 className='text-5xl leading-normal'>Providing the best experience to make your <span style={{color: '#3E60C9'}}>Online Shopping</span> </h1>
-                <p className='my-6 text-justify leading-10'>At Luminae, we are more than just an online store – we are your ultimate destination for an unparalleled shopping experience. Our journey began with a simple yet powerful idea: to create a platform that not only offers a wide array of products but also fosters a sense of community and connection among our customers.</p>
-                <div className='flex justify-between'>
-                    <div>
-                        <h3 className='text-4xl' style={{color:'#3E60C9'}}>20+</h3>
-                        <p>Shopping category</p>
+        <div className='section-container flex items-center my-10 gap-36'>
+            <div className='lg:w-2/3 md:w-2/3 sm:w-1/2'>
+                <p className='text-2xl sm:text-lg mb-4'>Let's get to know TechTrove</p>
+                <h1 className='text-5xl sm:text-3xl leading-normal'>Providing the best experience to make your <span style={{color: '#3E60C9'}}>Online Shopping</span> </h1>
+                <p className='my-6 sm:my:3 text-justify leading-10 sm:leading-7'>At Luminae, we are more than just an online store – we are your ultimate destination for an unparalleled shopping experience. Our journey began with a simple yet powerful idea: to create a platform that not only offers a wide array of products but also fosters a sense of community and connection among our customers.</p>
+                <div className='flex flex-row justify-between items-center gap-10'>
+                    <div className='text-left'>
+                        <h3 className='sm:text-2xl md:text-4xl lg:text-4xl mb-3' style={{color:'#3E60C9'}}>20+</h3>
+                        <p className='sm:text-xs md:text-lg lg:text-lg'>Shopping category</p>
                     </div>
-                    <div>
-                        <h3 className='text-4xl' style={{color:'#3E60C9'}}>10+</h3>
-                        <p>Different Territory</p>
+                    <div className='text-center'>
+                        <h3 className='sm:text-2xl md:text-4xl lg:text-4xl mb-3' style={{color:'#3E60C9'}}>10+</h3>
+                        <p className='sm:text-xs md:text-lg lg:text-lg'>Different Territory</p>
                     </div>
-                    <div>
-                        <h3 className='text-4xl' style={{color:'#3E60C9'}}>4M+</h3>
-                        <p>Happy Client</p>
+                    <div className='text-right'>
+                        <h3 className='sm:text-2xl md:text-4xl lg:text-4xl mb-3' style={{color:'#3E60C9'}}>4M+</h3>
+                        <p className='sm:text-xs md:text-lg lg:text-lg'>Happy Client</p>
                     </div>
                 </div>
             </div>
-            <div className='xl:w-2/3 lg:w-2/3 md:w-2/3 sm:w-1/2 '>
+            <div className='w-2/3 sm:w-1/2'>
                 <h3>We have made many people satisfied with our Platform</h3>
             </div>
         </div>
