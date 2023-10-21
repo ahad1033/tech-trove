5acac40a813be94cd6f8e948876eda911445fc7c
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-21 13:38:59 +0600

    banner section made responsive for all devices

--- src/Components/Home/HomeBanner/HomeBanner.jsx
+++ src/Components/Home/HomeBanner/HomeBanner.jsx
@@ -4,7 +4,7 @@ import { TbShoppingBagPlus } from 'react-icons/tb';
 const HomeBanner = () => {
     return (
         <div className='flex'>
-            <div className='lg:w-1/3 md:1/2 h-[547px] relative'>
+            <div className='relative w-1/2 lg:w-1/3'>
                 <img
                     src='https://i.ibb.co/hXjcCxW/banner-Left.jpg'
                     alt=""
@@ -20,9 +20,9 @@ const HomeBanner = () => {
                     </div>
                 </div>
             </div>
-            <div className='lg:w-2/3 md:w-1/2'>
+            <div className='w-1/2 lg:w-2/3'>
                 <div
-                    className='h-full object-cover lg:flex md:flex lg:flex-col md:flex-col items-center justify-center lg:gap-7 md:gap-4 text-white text-3xl lg:p-0 md:px-20'
+                    className='h-full object-cover flex flex-col items-center justify-center sm:gap-2 md:gap-4 lg:gap-7 text-white text-3xl sm:px-3 md:px-20 lg:px-0'
                     style={{
                         background: 'url(<path-to-image>)',
                         borderRadius: 'var(--spacing-0, 0px)',
@@ -32,9 +32,11 @@ const HomeBanner = () => {
                         backgroundColor: 'var(--button-default, #4172DC)',
                     }}
                 >
-                    <h1 className='text-4xl uppercase text-center'>Kimonos, Caftans & Pareos</h1>
-                    <h2 className='text-center'>Poolside glam included From $4.99</h2>
-                    <button className="button-primary"><TbShoppingBagPlus />Shop Now</button>
+                    <h1 className='text-lg md:text-4xl lg:text-4xl uppercase text-center'>Kimonos, Caftans & Pareos</h1>
+                    <h2 className='text-xs md:text-base lg:text-base text-center'>Poolside glam included From $4.99</h2>
+                    <div className='mx-auto'>
+                        <button className="button-primary-wide"><TbShoppingBagPlus />Shop Now</button>
+                    </div>
                 </div>
             </div>
         </div>
