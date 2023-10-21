2bf0336fc02a838bb41456625dd3a07a70923d9d
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-21 14:26:25 +0600

    flash sell section made responsive for all devices

--- src/Components/Trending/Trending.jsx
+++ src/Components/Trending/Trending.jsx
@@ -5,40 +5,40 @@ const Trending = () => {
     return (
         <div className='section-container'>
             <SectionTitle title={`Trending Must Have`} />
-            <div className='flex sm:flex-col md:flex-row lg:flex-row items-center justify-center gap-20'>
-                <div className='w-96'>
+            <div className='flex flex-col md:flex-row lg:flex-row items-center justify-center gap-10 lg:gap-20'>
+                <div className='w-96 px-10 md:px-0 lg:px-0'>
                     <img src="https://i.ibb.co/f4pC4jQ/4-org-zoom-1.png" alt="" />
-                    <div className='flex justify-between bottom-0 text-white p-4' style={{ backgroundColor: '#262626' }}>
+                    <div className='flex flex-col lg:flex-row justify-between bottom-0 text-white p-4' style={{ backgroundColor: '#262626' }}>
                         <div>
-                            <h1>Cool & Sexy Calvin Klein</h1>
-                            <p>Dotted dress-Casual</p>
+                            <h1 className='font-bold text-center lg:text-start'>Cool & Sexy Calvin Klein</h1>
+                            <p className='text-center lg:text-start'>Dotted dress-Casual</p>
                         </div>
-                        <div>
-                            <button className='btn btn-outline text-white'>$89 Shop Now</button>
+                        <div className='text-center mt-3 lg:mt-0'>
+                            <button className='btn btn-outline text-white btn-sm lg:btn-md'>$89 Shop Now</button>
                         </div>
                     </div>
                 </div>
-                <div className='w-96'>
+                <div className='w-96 px-10 md:px-0 lg:px-0'>
                     <img src="https://i.ibb.co/6RQhftg/4-org-zoom-1.png" alt="" />
-                    <div className='flex justify-between bottom-0 text-white p-4' style={{ backgroundColor: '#262626' }}>
+                    <div className='flex flex-col lg:flex-row justify-between bottom-0 text-white p-4' style={{ backgroundColor: '#262626' }}>
                         <div>
-                            <h1>Cool & Sexy Calvin Klein</h1>
-                            <p>Dotted dress-Casual</p>
+                            <h1 className='font-bold text-center lg:text-start'>Cool & Sexy Calvin Klein</h1>
+                            <p className='text-center lg:text-start'>Dotted dress-Casual</p>
                         </div>
-                        <div>
-                            <button className='btn btn-outline text-white'>$105 Shop Now</button>
+                        <div className='text-center mt-3 lg:mt-0'>
+                            <button className='btn btn-outline text-white btn-sm lg:btn-md'>$105 Shop Now</button>
                         </div>
                     </div>
                 </div>
-                <div className='w-96'>
+                <div className='w-96 px-10 md:px-0 lg:px-0'>
                     <img src="https://i.ibb.co/Pt6Qwx2/4-org-zoom-1.png" alt="" />
-                    <div className='flex justify-between bottom-0 text-white p-4' style={{ backgroundColor: '#262626' }}>
+                    <div className='flex flex-col lg:flex-row justify-between bottom-0 text-white p-4' style={{ backgroundColor: '#262626' }}>
                         <div>
-                            <h1>beige coat Zara</h1>
-                            <p>Cream-Brown-Formal</p>
+                            <h1 className='font-bold text-center lg:text-start'>beige coat Zara</h1>
+                            <p className='text-center lg:text-start'>Cream-Brown-Formal</p>
                         </div>
-                        <div>
-                            <button className='btn btn-outline text-white'>$70 Shop Now</button>
+                        <div className='text-center mt-3 lg:mt-0'>
+                            <button className='btn btn-outline text-white btn-sm lg:btn-md'>$70 Shop Now</button>
                         </div>
                     </div>
                 </div>
