080772ee14d4a941c304ee00e0972ce06a9f75c3
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-21 13:53:52 +0600

    search option modified

--- src/Components/Shared/NavBar/TopNavBar/SearchOption.jsx
+++ src/Components/Shared/NavBar/TopNavBar/SearchOption.jsx
@@ -6,7 +6,7 @@ const SearchOption = () => {
     return (
         <div className='hidden md:flex lg:flex items-center justify-center border border-1 rounded-xl gap-2'>
             <input type="text" placeholder="Search Products" className="input-xs input-ghost pl-2 border-none" />
-            <details className="dropdown">
+            <details className="dropdown hidden lg:block">
                 <summary className="mx-1 text-xs btn normal-case whitespace-nowrap">
                     <h4 className='flex flex-row items-center gap-2'>All Categories<span className="ml-1"><AiOutlineDown /></span></h4>
                 </summary>
