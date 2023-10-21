ef5d32ffc864124af0fda50e966933547b6eb3c3
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-21 23:20:06 +0600

    code mofified

--- src/Components/Home/BottomSection/BottomSection.jsx
+++ src/Components/Home/BottomSection/BottomSection.jsx
@@ -3,9 +3,9 @@ import { PiNavigationArrowFill } from 'react-icons/pi'
 
 const BottomSection = () => {
     return (
-        <div className='relative lg:mt-80 md:mt-28 sm:mt-16'>
+        <div className='relative mt-16 md:mt-28 lg:mt-80'>
             <div className='w-2/5 h-96 absolute inset-1/2 transform -translate-x-1/2  text-center px-36 py-20 rounded-xl -top-56  hidden lg:block' style={{ backgroundColor: '#7296AB' }}>
-                <h1 className='mb-3 text-5xl text-white'>Tech<span className='font-bold'>Trove</span></h1>
+                <h1 className='mb-10 text-5xl text-white'>Tech<span className='font-bold'>Trove</span></h1>
                 <p className='mb-6 text-white font-semibold'>Register your email not to miss the last minutes off+ Free delivery</p>
                 <div className="form-control">
                     <div className="input-group justify-center">
@@ -16,30 +16,30 @@ const BottomSection = () => {
                     </div>
                 </div>
             </div>
-            <div className='w-full items-center lg:px-60 md:px-40 sm:px-20 lg:pt-32 md:pt-24 sm:pt-10 lg:pb-32 md:pb-24 sm:pb-20 lg:gap-32 md:gap-14 sm:gap-10 grid lg:grid-cols-4 grid-cols-2 text-black' style={{ backgroundColor: '#d1e2eb' }}>
+            <div className='w-full grid grid-cols-2 lg:grid-cols-4  items-center px-3 md:px-40 lg:px-60 md:pt-15 lg:pt-32 md:pb-15 lg:pb-32 lg:gap-32 text-black text' style={{ backgroundColor: '#d1e2eb' }}>
                 <div className='items-start my-6 p-10'>
-                    <h3 className='font-bold text-lg'>Company</h3>
-                    <p className='my-4'>About Us</p>
-                    <p className='my-4'>Our Store</p>
-                    <p className='my-4'>Contact Us</p>
+                    <h3 className='font-bold text-sm md:text-lg lg:text-lg'>Company</h3>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>About Us</p>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>Our Store</p>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>Contact Us</p>
                 </div>
                 <div className='items-start my-6 p-10'>
-                    <h3 className='font-bold text-lg'>Career Opportunities</h3>
-                    <p className='my-4'>Selling Programs</p>
-                    <p className='my-4'>Advertise</p>
-                    <p className='my-4'>Cooperation </p>
+                    <h3 className='font-bold text-sm md:text-lg lg:text-lg'>Career</h3>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>Selling Programs</p>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>Advertise</p>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>Cooperation </p>
                 </div>
                 <div className='items-start my-6 p-10'>
-                    <h3 className='font-bold text-lg'>How to Buy</h3>
-                    <p className='my-4'>Making Payments</p>
-                    <p className='my-4'>Delivery Options</p>
-                    <p className='my-4'>New User Guide</p>
+                    <h3 className='font-bold text-sm md:text-lg lg:text-lg'>How to Buy</h3>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>Making Payments</p>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>Delivery Options</p>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>New User Guide</p>
                 </div>
                 <div className='items-start my-6 p-10'>
-                    <h3 className='font-bold text-lg'>Help</h3>
-                    <p className='my-4'>Contact Us</p>
-                    <p className='my-4'>FAQ</p>
-                    <p className='my-4'>Privacy Policy</p>
+                    <h3 className='font-bold text-sm md:text-lg lg:text-lg'>Help</h3>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>Contact Us</p>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>FAQ</p>
+                    <p className='my-4 text-xs md:text-sm lg:text-sm'>Privacy Policy</p>
                 </div>
             </div>
         </div>
