085ad0ac7bb09ae46b8584fc4466ff891deece67
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-23 01:57:28 +0600

    gallary section of the blog page made responsive for all devices

--- src/Components/Blog/BlogGalaray/BlogGalaray.jsx
+++ src/Components/Blog/BlogGalaray/BlogGalaray.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const BlogGalaray = () => {
     return (
-        <div className='section-container w-full mx-auto flex justify-center gap-5 lg:my-16 md:my-10 sm:my-8'>
-            <div className='w-2/3'>
-                <img className='w-full h-full' src="https://i.ibb.co/VgjVgY0/blog-Galary1.png" alt="" />
+        <div className='section-container w-full mx-auto flex justify-center gap-5 lg:my-16 md:my-10 sm:my-8 px-5 md:px-10 lg:px-0 pb-5 md:pb-0 lg:pb-0 '>
+            <div className='w-2/3 object-center'>
+                <img className='h-full object-cover' src="https://i.ibb.co/VgjVgY0/blog-Galary1.png" alt="" />
             </div>
             <div className='w-1/3 flex flex-col gap-5'>
-                <img className='w-full' src="https://i.ibb.co/6nrV9Xj/blog-Galary2.png" alt="" />
-                <img className='w-full' src="https://i.ibb.co/y007txw/blog-Galary4.png" alt="" />
-                <img className='w-full' src="https://i.ibb.co/gvTKcP7/blog-Galary3.png" alt="" />
+                <img className='h-full object-cover' src="https://i.ibb.co/6nrV9Xj/blog-Galary2.png" alt="" />
+                <img className='h-full object-cover' src="https://i.ibb.co/y007txw/blog-Galary4.png" alt="" />
+                <img className='h-full object-cover' src="https://i.ibb.co/gvTKcP7/blog-Galary3.png" alt="" />
             </div>
         </div>
     );
