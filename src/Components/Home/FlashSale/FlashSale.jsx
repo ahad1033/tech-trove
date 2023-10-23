3d00de78da6d50cb23cde55c5dcb538d75311031
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-24 01:07:18 +0600

    falsh sales json file added and fetched to the flash sale section

--- src/Components/Home/FlashSale/FlashSale.jsx
+++ src/Components/Home/FlashSale/FlashSale.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../Shared/SectionTitle/SectionTitle';
 
 const FlashSale = () => {
+
+    const [ product, setProduct] = useState([]);
+
+    useEffect( () => {
+        fetch('flashSales.json')
+        .then (response => response.json())
+        .then (data => setProduct(data))
+    },[])
+
     return (
         // TO DO.....
         <section className='section-container'>
             <SectionTitle title={"Flash Sale"} />
             <div>
-                <h1 className="text-5xl text-center">Flash sell will be here~soon.</h1>
+                
             </div>
         </section>
     );
