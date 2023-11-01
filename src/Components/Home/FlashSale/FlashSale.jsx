1998e9d3b7010bb8838e75196213224d122e02a0
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-01 11:35:15 +0600

    top 100 json file created

--- src/Components/Home/FlashSale/FlashSale.jsx
+++ src/Components/Home/FlashSale/FlashSale.jsx
@@ -26,3 +26,49 @@ const FlashSale = () => {
 };
 
 export default FlashSale;
+
+// import React, { useEffect, useState } from 'react';
+// import SectionTitle from '../../Shared/SectionTitle/SectionTitle';
+// import Product from './Product';
+// import Swiper from 'swiper';
+
+// import 'swiper/swiper-bundle.css';
+
+// const FlashSale = () => {
+//   const [products, setProducts] = useState([]);
+//   const swiperRef = React.createRef();
+
+//   useEffect(() => {
+//     fetch('flashSales.json')
+//       .then((res) => res.json())
+//       .then((data) => setProducts(data))
+//       .then(() => {
+//         // Initialize Swiper when products data is available
+//         new Swiper(swiperRef.current, {
+//           // Swiper options and configuration here
+//           slidesPerView: 3,
+//           spaceBetween: 10,
+//           // Add more options as needed
+//         });
+//       });
+//   }, []);
+
+//   return (
+//     <section className="section-container">
+//       <SectionTitle title={"Flash Sale"} />
+//       <div className="swiper-container" ref={swiperRef}>
+//         <div className="swiper-wrapper">
+//           {products.map((product) => (
+//             <div className="swiper-slide" key={product.id}>
+//               <Product product={product} />
+//             </div>
+//           ))}
+//         </div>
+//         {/* <div className="swiper-button-next"></div>
+//         <div className="swiper-button-prev"></div> */}
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default FlashSale;
