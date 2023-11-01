677e9af8c59beb70d911f7f3df574cc9f44cab54
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-01 11:42:44 +0600

    fetchend the top hundred product on the home page

--- src/Components/Home/TopHundred/TopHundred.jsx
+++ src/Components/Home/TopHundred/TopHundred.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import TopProduct from './TopProduct';
 
 const TopHundred = () => {
+
+    const [topProducts, setTopProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('topHundred.json')
+            .then(res => res.json())
+            .then(data => setTopProducts(data))
+    }, [])
+
+
     return (
         <div className='section-container'>
-            <h1 className="text-5xl text-center mt-20">Top100 will be here~soon</h1> 
+            <h1 className="text-5xl text-center mt-20">Top100 will be here~soon</h1>
+            {
+                topProducts.map(product => <TopProduct product={product} key={product.id} />)
+            }
         </div>
     );
 };
