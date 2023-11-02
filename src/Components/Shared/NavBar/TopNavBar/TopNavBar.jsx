53f8de450007fd2b78d9224f5330bf03f0ea6dc5
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-02 11:07:16 +0600

    some bug fixes

--- src/Components/Shared/NavBar/TopNavBar/TopNavBar.jsx
+++ src/Components/Shared/NavBar/TopNavBar/TopNavBar.jsx
@@ -7,12 +7,12 @@ import SocialIcons from './SocialIcons';
 const TopNavBar = () => {
 
     return (
-        <div className="navbar flex flex-row-reverse md:flex-col lg:flex-row justify-between bg-white text-black section-container sm:py-8 md:py-3 lg:py-7">
+        <div className="navbar flex flex-row-reverse md:flex-col lg:flex-row justify-between bg-white text-black section-container py-8 md:py-3 lg:py-7">
             <div className="w-full flex flex-row gap-3 items-center justify-between">
                 <Logo />
                 <SearchOption />
             </div>
-            <div className="flex md:mt-4 justify-between w-full ">
+            <div className="flex mt-0 md:mt-4 lg:mt-0 justify-between w-full items-center">
                 <TopNavOptions />
                 <SocialIcons />
             </div>
