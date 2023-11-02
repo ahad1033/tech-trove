1c4945215141ce6644707fa8fee181511c1e0f6f
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-02 10:39:52 +0600

    top 100section added in the home page

--- src/Components/Home/TopHundred/TopHundred.jsx
+++ src/Components/Home/TopHundred/TopHundred.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import TopProduct from './TopProduct';
+import SectionTitle from '../../Shared/SectionTitle/SectionTitle';
 
 const TopHundred = () => {
 
@@ -8,16 +9,20 @@ const TopHundred = () => {
     useEffect(() => {
         fetch('topHundred.json')
             .then(res => res.json())
-            .then(data => setTopProducts(data))
-    }, [])
-
+            .then(data => {
+                setTopProducts(data);
+            });
+    }, []);
 
     return (
         <div className='section-container'>
-            <h1 className="text-5xl text-center mt-20">Top100 will be here~soon</h1>
-            {
-                topProducts.map(product => <TopProduct product={product} key={product.id} />)
-            }
+            {/* <h1 className="text-5xl text-center mt-20">Top100 will be here soon</h1> */}
+            <SectionTitle title="Top 100" />
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10'>
+                {
+                    topProducts.map((product) => <TopProduct product={product} key={product.id} />)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Home/TopHundred/TopProduct.jsx
+++ src/Components/Home/TopHundred/TopProduct.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const TopProduct = () => {
+const TopProduct = ( {product} ) => {
+    
+    const { companyName, productDetails, productImg, ratings, price, discountPrice} = product;
+
     return (
-        <div>
-            
+        <div className="card w-80 bg-base-100 shadow-xl justify-center"> 
+            <img className='' src={productImg} alt="Top100" />
+            <div className="card-body px-16">
+                <h2 className="card-title">{companyName}</h2>
+                <p>{productDetails}</p>
+                <div className='flex'>
+                    <p className='line-through'>${price}</p>
+                    <p className='text-right text-red-500'>${discountPrice}</p>
+                </div>
+                {/* <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div> */}
+            </div>
         </div>
     );
 };
