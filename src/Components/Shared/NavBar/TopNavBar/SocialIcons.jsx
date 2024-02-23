7fa60852e4004693a823a1f967c07c51b22202c3
ahad1033 <a.ahad2232@gmail.com>
2024-02-24 04:23:05 +0600

    added the links for navigation bars icon

--- src/Components/Shared/NavBar/TopNavBar/SocialIcons.jsx
+++ src/Components/Shared/NavBar/TopNavBar/SocialIcons.jsx
@@ -2,11 +2,21 @@ import { AiFillInstagram } from 'react-icons/ai';
 import { BiLogoTelegram, BiLogoFacebook } from 'react-icons/bi';
 
 const SocialIcons = () => {
+    const instagramLink = "https://www.instagram.com/";
+    const facebookLink = "https://www.facebook.com/";
+    const telegramLink = "https://web.telegram.org/k/";
+
     return (
         <div className='hidden md:flex lg:flex gap-2 text-gray-400'>
-            <AiFillInstagram className='text-2xl' />
-            <BiLogoFacebook className='text-2xl' />
-            <BiLogoTelegram className='text-2xl' />
+            <a href={instagramLink} target="_blank" rel="noopener noreferrer">
+                <AiFillInstagram className='text-2xl' />
+            </a>
+            <a href={facebookLink} target="_blank" rel="noopener noreferrer">
+                <BiLogoFacebook className='text-2xl' />
+            </a>
+            <a href={telegramLink} target="_blank" rel="noopener noreferrer">
+                <BiLogoTelegram className='text-2xl' />
+            </a>
         </div>
     );
 };
