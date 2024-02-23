5ce5feb86a91db420808ca5f6c4e7984fac18c6a
ahad1033 <a.ahad2232@gmail.com>
2024-02-24 04:18:11 +0600

    installed redux for state management and modified the ui

--- src/Components/Home/FlashSale/FlashSale.jsx
+++ src/Components/Home/FlashSale/FlashSale.jsx
@@ -1,74 +1,30 @@
-import { useEffect, useState } from 'react';
-import SectionTitle from '../../Shared/SectionTitle/SectionTitle';
-import Product from './Product';
+import { useEffect, useState } from "react";
+import Product from "./Product";
 
 const FlashSale = () => {
+  const [products, setProducts] = useState([]);
 
-    const [ products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("flashSales.json")
+      // fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    useEffect( () => {
-        fetch('flashSales.json')
-        .then (res => res.json())
-        .then (data => setProducts(data))
-    },[])
-    // console.log(products);
-    return (
-        // TO DO.....
-        <section className='section-container'>
-            <SectionTitle title={"Flash Sale"} />
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10'>
-                {
-                    products.map(product => <Product product={product} key={product.id}  />)
-                }
-            </div>
-        </section>
-    );
+  return (
+    // TO DO.....
+    <section className="section-container">
+      <div className="flex justify-between items-center my-12">
+        <h3 className="text-2xl mb-2">Flash Sale</h3>
+        <button className="btn text-white btn-link">view all</button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+        {products.map((product) => (
+          <Product product={product} key={product.id} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default FlashSale;
-
-// import React, { useEffect, useState } from 'react';
-// import SectionTitle from '../../Shared/SectionTitle/SectionTitle';
-// import Product from './Product';
-// import Swiper from 'swiper';
-
-// import 'swiper/swiper-bundle.css';
-
-// const FlashSale = () => {
-//   const [products, setProducts] = useState([]);
-//   const swiperRef = React.createRef();
-
-//   useEffect(() => {
-//     fetch('flashSales.json')
-//       .then((res) => res.json())
-//       .then((data) => setProducts(data))
-//       .then(() => {
-//         // Initialize Swiper when products data is available
-//         new Swiper(swiperRef.current, {
-//           // Swiper options and configuration here
-//           slidesPerView: 3,
-//           spaceBetween: 10,
-//           // Add more options as needed
-//         });
-//       });
-//   }, []);
-
-//   return (
-//     <section className="section-container">
-//       <SectionTitle title={"Flash Sale"} />
-//       <div className="swiper-container" ref={swiperRef}>
-//         <div className="swiper-wrapper">
-//           {products.map((product) => (
-//             <div className="swiper-slide" key={product.id}>
-//               <Product product={product} />
-//             </div>
-//           ))}
-//         </div>
-//         {/* <div className="swiper-button-next"></div>
-//         <div className="swiper-button-prev"></div> */}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default FlashSale;
--- src/Components/Home/TopHundred/TopHundred.jsx
+++ src/Components/Home/TopHundred/TopHundred.jsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import TopProduct from './TopProduct';
-import SectionTitle from '../../Shared/SectionTitle/SectionTitle';
+import { useEffect, useState } from "react";
+import TopProduct from "./TopProduct";
 
 const TopHundred = () => {
+  const [topProducts, setTopProducts] = useState([]);
 
-    const [topProducts, setTopProducts] = useState([]);
+  useEffect(() => {
+    fetch("topHundred.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setTopProducts(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch('topHundred.json')
-            .then(res => res.json())
-            .then(data => {
-                setTopProducts(data);
-            });
-    }, []);
-
-    return (
-        <div className='section-container'>
-            {/* <h1 className="text-5xl text-center mt-20">Top100 will be here soon</h1> */}
-            <SectionTitle title="Top 100" />
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mx-auto'>
-                {
-                    topProducts.map((product) => <TopProduct product={product} key={product.id} />)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className="section-container">
+      <div className="flex justify-between items-center my-12">
+        <h3 className="text-2xl mb-2">Top 100</h3>
+        <button className="btn btn-active text-white btn-link">view all</button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mx-auto">
+        {topProducts.map((product) => (
+          <TopProduct product={product} key={product.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TopHundred;
